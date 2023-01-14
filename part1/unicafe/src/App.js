3fd8aa10d877fde7d92cf423ba8b10608186ac5b
in-key <inkeydev@gmail.com>
2023-01-13 20:45:04 -0500

    unicafe 1.7-1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,7 +2,20 @@ import { useState } from 'react'
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>;
 
-const Stat = ({text, stat}) => <div>{text} {stat}</div>;
+const Stat = ({props}) => {
+  const {good, neutral, bad} = props;
+  const all = good + neutral + bad;
+  return (
+    <>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {(good - bad) / all}</div>
+      <div>positive {good / all * 100}%</div>
+    </>
+  )
+}
 
 
 const App = () => {
@@ -18,9 +31,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text={'neutral'}/>
       <Button onClick={() => setBad(bad + 1)} text={'bad'}/>
       <h1>statistics</h1>
-      <Stat text={'good'} stat={good}/>
-      <Stat text={'neutral'} stat={neutral}/>
-      <Stat text={'bad'} stat={bad}/>
+      <Stat props={{good, neutral, bad}}/>
     </div>
   )
 }
