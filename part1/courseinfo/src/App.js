8da3bb93fdbc3b8277de70ae3ddb89855633fdc6
in-key <inkeydev@gmail.com>
2023-01-13 19:29:22 -0500

    complete exercise 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -5,7 +5,7 @@ const Header = ({course}) => {
 }
 
 const Content = ({parts}) => {
-  const {part1, part2, part3} = parts;
+  const [part1, part2, part3] = parts;
   return (
     <div>
       <Part part={part1}/>
@@ -22,8 +22,7 @@ const Part = ({part}) => {
 }
 
 const Total = ({parts}) => {
-  const {part1, part2, part3} = parts;
-
+  const [part1, part2, part3] = parts;
   return (
     <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
   )
@@ -31,24 +30,27 @@ const Total = ({parts}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={{part1, part2, part3}}/>
-      <Total parts={{part1, part2, part3}}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
