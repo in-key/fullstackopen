4a3e3b5a98a30c5d065d8122dc4f320916d4a3cd
in-key <inkeydev@gmail.com>
2023-01-17 00:59:00 -0500

    4.13-4.14 bloglist more routes/tests

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -17,4 +17,20 @@ blogRouter.post('/', async (request, response) => {
     response.status(201).json(result)
 })
 
+blogRouter.delete('/:id', async (req, res) => {
+    const id = req.params.id
+
+    await Blog.findByIdAndDelete(id)
+
+    res.status(204).end()
+})
+
+blogRouter.put('/:id', async (req, res) => {
+    const id = req.params.id
+
+    const returnedBlog = await Blog.findByIdAndUpdate(id, req.body, {new: true})
+
+    res.json(returnedBlog)
+})
+
 module.exports = blogRouter;
--- part4/bloglist/tests/bloglist.test.js
+++ part4/bloglist/tests/bloglist.test.js
@@ -99,6 +99,38 @@ test('if the title or url properties are missing from the request data, the back
         .expect(400)
 })
 
+test('deleting a single blog post resource works', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map( b => b.title )
+
+    expect(titles).not.toContain(blogToDelete.title)
+})
+
+test('updating the number of likes of an existing blog works', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+
+    await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send({ likes: 60 })
+        .expect(200)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const returnedBlog = blogsAtEnd.find(blog => blog.title === blogToUpdate.title)
+
+    expect(returnedBlog.likes).toBe(60)
+})
+
 afterAll(() => {
     mongoose.connection.close();
 })
