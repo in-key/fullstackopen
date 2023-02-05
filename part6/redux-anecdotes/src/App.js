9d89da4db0930095b6299751d535bf53ce7b1da7
in-key <inkeydev@gmail.com>
2023-02-05 12:35:08 -0500

    6.3-6.4 redux-anecdotes vote & create new anecdote

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,44 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch({ type: 'VOTE', payload: { id: id } })
+  }
+
+  const handleCreate = (e) => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    e.target.anecdote.value = ''
+    dispatch({ type: 'CREATE', payload: { content: content } })
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={handleCreate}>
+        <div>
+          <input name='anecdote'/>
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
