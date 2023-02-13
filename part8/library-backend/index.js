383eebc893324fe1b2226b9167750a68d50da4f9
in-key <inkeydev@gmail.com>
2023-02-13 13:50:57 -0500

    8.5 add optional param genre to allAuthors and can be used with author param

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -126,8 +126,17 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) return books
-      return books.filter((b) => b.author === args.author)
+      if (args.author && args.genre) {
+        return books.filter(
+          (b) => b.author === args.author && b.genres.includes(args.genre)
+        )
+      } else if (args.author) {
+        return books.filter((b) => b.author === args.author)
+      } else if (args.genre) {
+        return books.filter((b) => b.genres.includes(args.genre))
+      } else {
+        return books
+      }
     },
     allAuthors: () => authors,
   },
