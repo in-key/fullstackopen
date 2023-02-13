04f5bcdd35bb3927fef368eff192bbd6754e23aa
in-key <inkeydev@gmail.com>
2023-02-13 17:40:13 -0500

    8.10 rerender authors & books after adding new book

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useMutation } from "@apollo/client"
-import { ADD_BOOK, ALL_BOOKS } from "../queries"
+import { ADD_BOOK, ALL_BOOKS, ALL_AUTHORS } from "../queries"
 
 const NewBook = (props) => {
   const [title, setTitle] = useState("")
@@ -10,7 +10,7 @@ const NewBook = (props) => {
   const [genres, setGenres] = useState([])
 
   const [addBook] = useMutation(ADD_BOOK, {
-    refetchQueries: [{ query: ALL_BOOKS }],
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
   })
 
   if (!props.show) {
