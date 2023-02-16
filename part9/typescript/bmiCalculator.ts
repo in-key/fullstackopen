3e36f3730226354bf7253ba3512312c5057579e4
in-key <inkeydev@gmail.com>
2023-02-16 18:29:32 -0500

    9.1 Body mass index

---
+++ part9/typescript/bmiCalculator.ts
@@ -0,0 +1,25 @@
+function calculateBmi(height: number, weight: number) {
+  const bmi = weight / Math.pow(height / 100, 2);
+
+  if (bmi < 16) {
+    return "Underweight (Severe thinness)";
+  } else if (bmi < 17) {
+    return "Underweight (Moderate thinness)";
+  } else if (bmi < 18.5) {
+    return "Underweight (Mild thinness)";
+  } else if (bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi < 30) {
+    return "Overweight (Pre-obese)";
+  } else if (bmi < 35) {
+    return "Obese (Class I)";
+  } else if (bmi < 40) {
+    return "Obese (Class II)";
+  } else if (bmi >= 40) {
+    return "Obese (Class III)";
+  } else {
+    return "Invalid input";
+  }
+}
+
+console.log(calculateBmi(180, 74));
