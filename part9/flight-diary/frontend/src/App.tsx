7c58c14d09620ac20387ac86f76572c4ceea5350
in-key <inkeydev@gmail.com>
2023-02-19 16:49:28 -0500

    9.17 add way to add new diary from frontend

--- part9/flight-diary/frontend/src/App.tsx
+++ part9/flight-diary/frontend/src/App.tsx
@@ -1,24 +1,76 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { diaryEntry } from "./types";
 import diaryService from "./diaryService";
+import axios from "axios";
 
 function App() {
   const [diaries, setDiaries] = useState<diaryEntry[]>([]);
+  const [date, setDate] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [weather, setWeather] = useState("");
+  const [comment, setComment] = useState("");
 
   useEffect(() => {
     diaryService.getAll().then((data) => setDiaries(data));
-  });
+  }, []);
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const addedDiary = await diaryService.addNewDiary({
+      date,
+      visibility,
+      weather,
+      comment,
+    });
+    setDate("");
+    setVisibility("");
+    setWeather("");
+    setComment("");
+    setDiaries(diaries.concat(addedDiary));
+  };
   return (
     <div>
-      {" "}
-      <h1>Diary Entries</h1>
-      {diaries.map((diary) => (
-        <div key={diary.id}>
-          <h2>{diary.date}</h2>
-          <div>visibility: {diary.visibility}</div>
-          <div>weather: {diary.weather}</div>
-        </div>
-      ))}
+      <div>
+        <h1>Add new entry</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            date{" "}
+            <input value={date} onChange={(e) => setDate(e.target.value)} />
+          </div>
+          <div>
+            visibility{" "}
+            <input
+              value={visibility}
+              onChange={(e) => setVisibility(e.target.value)}
+            />
+          </div>
+          <div>
+            weather{" "}
+            <input
+              value={weather}
+              onChange={(e) => setWeather(e.target.value)}
+            />
+          </div>
+          <div>
+            comment{" "}
+            <input
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+          </div>
+          <button>submit</button>
+        </form>
+      </div>
+      <div>
+        <h1>Diary Entries</h1>
+        {diaries.map((diary) => (
+          <div key={diary.id}>
+            <h2>{diary.date}</h2>
+            <div>visibility: {diary.visibility}</div>
+            <div>weather: {diary.weather}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- part9/flight-diary/frontend/src/diaryService.tsx
+++ part9/flight-diary/frontend/src/diaryService.tsx
@@ -2,11 +2,24 @@ import axios from "axios";
 import { diaryEntry } from "./types";
 const baseUrl = "http://localhost:3001/api/diaries";
 
+interface newDiary {
+  date: string;
+  weather: string;
+  visibility: string;
+  comment: string;
+}
+
 const getAll = async () => {
   const res = await axios.get<diaryEntry[]>(baseUrl);
   return res.data;
 };
 
+const addNewDiary = async (diary: newDiary) => {
+  const res = await axios.post<diaryEntry>(baseUrl, diary);
+  return res.data;
+};
+
 export default {
   getAll,
+  addNewDiary,
 };
